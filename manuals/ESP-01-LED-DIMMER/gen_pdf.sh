a598faeae344d24959f24408b15575f8521ff931
kuba <qb4.dev@gmail.com>
2024-12-29 19:59:56 +0100

    added first manual

---
+++ manuals/ESP-01-LED-DIMMER/gen_pdf.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+mkdir -p pdf
+
+asciidoctor-pdf \
+-a pdf-fontsdir=../adoc_common/fonts \
+-a pdf-themesdir=../adoc_common/theme \
+-a pdf-theme=supla \
+-a lang=pl \
+-a device=ESP-01-LED-DIMMER \
+document_main.adoc -o pdf/ESP-01-LED-DIMMER.pdf
