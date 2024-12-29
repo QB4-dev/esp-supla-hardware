e9a2c5f8b3301d0bdcc584f60b1490c937890b80
kuba <qb4.dev@gmail.com>
2024-12-29 21:15:30 +0100

    DIMMER manual updated

--- manuals/ESP-01-LED-DIMMER/gen_pdf.sh
+++ manuals/ESP-01-LED-DIMMER/gen_pdf.sh
@@ -8,4 +8,12 @@ asciidoctor-pdf \
 -a pdf-theme=supla \
 -a lang=pl \
 -a device=ESP-01-LED-DIMMER \
-document_main.adoc -o pdf/ESP-01-LED-DIMMER.pdf
+document_main.adoc -o pdf/ESP-01-LED-DIMMER_PL.pdf
+
+asciidoctor-pdf \
+-a pdf-fontsdir=../adoc_common/fonts \
+-a pdf-themesdir=../adoc_common/theme \
+-a pdf-theme=supla \
+-a lang=en \
+-a device=ESP-01-LED-DIMMER \
+document_main.adoc -o pdf/ESP-01-LED-DIMMER_EN.pdf
